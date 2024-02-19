b25aaaa109a2628b577df912cc52d6229935f6d3
David Inoa <davidinoa@gmail.com>
2024-02-19 16:38:47 -0400

    feat: allow to import svgs as react components

--- next.config.js
+++ next.config.js
@@ -2,9 +2,33 @@
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
  */
-await import("./src/env.js");
+await import('./src/env.js')
 
 /** @type {import("next").NextConfig} */
-const config = {};
+const config = {
+  webpack(config) {
+    const fileLoaderRule = config.module.rules.find(
+      (/** @type {{ test: { test: (arg0: string) => any; }; }} */ rule) =>
+        rule.test?.test?.('.svg'),
+    )
 
-export default config;
+    config.module.rules.push(
+      {
+        ...fileLoaderRule,
+        test: /\.svg$/i,
+        resourceQuery: /url/, // *.svg?url
+      },
+      {
+        test: /\.svg$/i,
+        issuer: fileLoaderRule.issuer,
+        resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] },
+        use: ['@svgr/webpack'],
+      },
+    )
+
+    fileLoaderRule.exclude = /\.svg$/i
+    return config
+  },
+}
+
+export default config
