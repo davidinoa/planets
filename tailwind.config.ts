7e2b15d0dc13374946a0d65244e37a3cbbe76efe
David Inoa <davidinoa@gmail.com>
2024-02-20 18:29:51 -0400

    refactor: rename colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,22 +2,22 @@ import { fontFamily } from 'tailwindcss/defaultTheme'
 import type { Config } from 'tailwindcss/types/config'
 
 export default {
-  content: ['./src/**/*.tsx'],
+  content: ['./src/**/*.tsx', './src/**/*.ts'],
   theme: {
     extend: {
       colors: {
+        mercury: '#419EBB',
+        venus: '#EDA249',
+        earth: '#6D2ED5',
+        mars: '#D14C32',
+        uranus: '#1EC1A2',
+        neptune: '#2D68F0',
+        saturn: '#CD5120',
+        jupiter: '#D83A34',
         'midnight-ink': 'var(--color-midnight-ink)',
         'cosmic-slate': '#38384F',
         'lunar-smoke': '#838391',
-        'skyline-blue': '#419EBB',
-        'aqua-dream': '#1EC1A2',
-        'royal-azure': '#2D68F0',
-        'purple-mystery': '#6D2ED5',
-        'terra-cotta': '#CD5120',
-        'sunset-coral': '#D14C32',
-        'firebrick-red': '#D83A34',
         'silver-lining': '#D8D8D8',
-        'golden-saffron': '#EDA249',
       },
       fontFamily: {
         sans: ['var(--font-sans)', ...fontFamily.sans],
