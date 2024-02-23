02e45f281fde72a2eb2a4f8039d99adf054877c5
David Inoa <davidinoa@gmail.com>
2024-02-22 20:16:08 -0400

    feat(fonts): set display property to block

--- src/ui/fonts.ts
+++ src/ui/fonts.ts
@@ -3,9 +3,11 @@ import { Antonio, League_Spartan as Spartan } from 'next/font/google'
 export const antonio = Antonio({
   subsets: ['latin'],
   variable: '--font-antonio',
+  display: 'block',
 })
 
 export const spartan = Spartan({
   subsets: ['latin'],
   variable: '--font-spartan',
+  display: 'block',
 })
