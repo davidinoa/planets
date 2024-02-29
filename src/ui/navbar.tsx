4c02312573a7acc7591f5419d35fd9d066cf9b94
David Inoa <davidinoa@gmail.com>
2024-02-29 15:45:11 -0400

    feat: create component for the animated links

--- src/ui/navbar.tsx
+++ src/ui/navbar.tsx
@@ -34,31 +34,7 @@ export default function Navbar() {
           <ul className="hidden gap-8 md:flex md:items-center">
             {PLANETS.map((planet) => (
               <li key={planet.id}>
-                <motion.a
-                  href={`/${planet.id}`}
-                  initial="idle"
-                  whileHover="hover"
-                  className="relative inline-block text-[0.6875rem] font-bold uppercase leading-[2.15] tracking-[1px]"
-                >
-                  {planet.name}
-                  <svg height="4" width="100%" className="absolute">
-                    <motion.path
-                      d="M0,1 L100,1"
-                      strokeWidth="4"
-                      variants={{
-                        idle: {
-                          pathLength: 0,
-                          transition: { duration: 0.1 },
-                        },
-                        hover: {
-                          pathLength: 1,
-                          transition: { duration: 0.5 },
-                          stroke: `var(--color-${planet.id})`,
-                        },
-                      }}
-                    />
-                  </svg>
-                </motion.a>
+                <AnimatedPlanetLink planet={planet} />
               </li>
             ))}
           </ul>
@@ -129,4 +105,39 @@ function useScrollAnimation(): MotionStyle {
   }, [scrollY, y])
 
   return { y } as const
+}
+
+type Planet = (typeof PLANETS)[number]
+
+function AnimatedPlanetLink({ planet }: { planet: Planet }) {
+  return (
+    <motion.a
+      href={`/${planet.id}`}
+      initial="idle"
+      whileHover="hover"
+      variants={{
+        hover: { opacity: 0.7, transition: { duration: 0.3 } },
+      }}
+      className="relative inline-block text-[0.6875rem] font-bold uppercase leading-[2.15] tracking-[1px]"
+    >
+      {planet.name}
+      <svg height="4" width="100%" className="absolute">
+        <motion.path
+          d="M0,1 L100,1"
+          strokeWidth="4"
+          variants={{
+            idle: {
+              pathLength: 0,
+              transition: { duration: 0.1 },
+            },
+            hover: {
+              pathLength: 1,
+              transition: { duration: 0.5 },
+              stroke: `var(--color-${planet.id})`,
+            },
+          }}
+        />
+      </svg>
+    </motion.a>
+  )
 }
